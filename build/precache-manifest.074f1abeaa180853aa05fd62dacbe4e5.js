310f1637a5649cc94700459e0ff3af728d7d0219
RickyBic <99736616+RickyBic@users.noreply.github.com>
2023-02-03 20:31:52 +0300

    Update @Final-20:30

---
+++ build/precache-manifest.074f1abeaa180853aa05fd62dacbe4e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "60bd76cb5ad45a67163f",
+    "url": "/static/css/main.cffb006b.chunk.css"
+  },
+  {
+    "revision": "60bd76cb5ad45a67163f",
+    "url": "/static/js/main.60bd76cb.chunk.js"
+  },
+  {
+    "revision": "2af8e7df2f75b3678b96",
+    "url": "/static/js/1.2af8e7df.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "05486c88f0edb55aad3e02f4a664b368",
+    "url": "/index.html"
+  }
+];
